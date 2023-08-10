0eecbcc83b74abd74055e0cc214aa5c35fedcd2f
mouryaaadarsh <141898966+mouryaaadarsh@users.noreply.github.com>
2023-08-10 21:50:58 +0530

    Add files via upload

---
+++ tempreture.js
@@ -0,0 +1,26 @@
+const CelciusE1=document.getElementById("Celcius");
+const FahrenheitE1=document.getElementById("Fahrenheit");
+const KelvinE1=document.getElementById("Kelvin");
+function computeTemp()
+{
+    //Add to avoid string poblem
+     const currentValue=event.target.value;
+
+     switch(event.target.name)
+     {
+        case "Celcius":
+            KelvinE1.value=currentValue+273.15;
+            FahrenheitE1.value=currentValue * 1.8 + 32;
+            break;
+            case "Fahrenheit":
+            CelciusE1.value=(currentValue - 32) / 1.8;
+            KelvinE1.value=(currentValue -32) / 1.8 + 273.15;
+            break;
+            case "Kelvin":
+            FahrenheitE1.value=currentValue-273.15;
+            FahrenheitE1.value=(currentValue- 273.15) * 1.8 + 32;
+            break;
+            default:
+                break;
+}
+}
